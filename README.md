f3587256ddb5242a728fd36998ffefa081cdb1dc
JimWails <1142247734@qq.com>
2020-03-17 11:19:16 +0800

    Updata

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # JointBan
 FiveM (CitizenFX) 国服联合封禁插件
 
+说明：
 此插件作用就是防止那些捣乱的玩家祸害服务器，如果自己服务器内有捣乱的，请记录下它的Steam ID然后通过Pull request上报或者在联盟群内发布相关证据及Steam ID，核实后第一时间同步全网更新，让捣乱的玩家不能继续祸害其他服务器
+
+使用方法：
+1.下载此插件
+2.将服务器插件文件夹内的JointBan文件夹内解压至服务器端resources目录
+3.在server.cfg(服务器配置文件)的自动启动处添加 ensure JointBan
+4.重启服务器端即可
---
+++ 服务器插件/JointBan/server.lua
@@ -0,0 +1,51 @@
+local blacklist = ""
+
+function isBlacklisted(id)
+	blacklist = LoadResourceFile(GetCurrentResourceName(), "data/blacklist.txt") or ""
+	i,j = string.find(blacklist, id)
+	if i ~= nil and j ~= nil then
+	    return true
+	else 
+	    return false
+	end
+end
+
+AddEventHandler("playerConnecting", function(name, reject, def)
+	local source	= source
+	local steamID = GetSteamID(source)
+
+	if not steamID then
+		reject("检测到您未开启Steam，请先运行Steam然后重启FiveM (CitizenFX)后再次尝试加入本服务器.Detected that you did not started Steam, please run Steam first and restart FiveM (CitizenFX), then try to join this server .")
+		CancelEvent()
+		return
+	end
+	for k,v in ipairs(GetPlayerIdentifiers(source))do
+	if isBlacklisted(GetPlayerIdentifiers(source)[1]) then
+		reject("国服联合封禁：此Steam ID因违反规定已被本服务器或其他服务器永久封禁，禁止加入本服务器")
+		CancelEvent()
+		break
+	end
+	end
+end)
+
+function GetSteamID(src)
+	local sid = GetPlayerIdentifiers(src)[1] or false
+
+	if (sid == false or sid:sub(1,5) ~= "steam") then
+		return false
+	end
+
+	return sid
+end
+
+function CheckVersion()
+	print("Checking Blacklist...\n")
+	PerformHttpRequest("https://github.com", function(err, rText, headers)
+end
+
+Citizen.CreateThread(function()
+	while true do
+		CheckVersion()
+		Citizen.Wait(120000)
+	end
+end)
