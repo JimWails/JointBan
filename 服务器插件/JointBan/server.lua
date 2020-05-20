03013193b0f46acaad4e11e1f3dce655a03ace79
JimWails <1142247734@qq.com>
2020-05-20 10:23:54 +0800

    Update 1.0.1.6
    新增R星ID检测功能（更严格的封禁措施，可在脚本内自行决定是否开启）

--- 服务器插件/JointBan/server.lua
+++ 服务器插件/JointBan/server.lua
@@ -1,7 +1,12 @@
 local blacklist = ""
-local APIRequest = "https://raw.githubusercontent.com/JimWails/JointBan/master/BanData"
+local rblacklist = ""
+local APIRequest = "http://api.kwfrb.club/BanData"
 local APTRetryTimes = 0
-local ShowUpdataInfo = true
+
+local AutoCheckTime = 120 -- 可自行决定多久进行一次自动更新封禁数据的时间,默认120秒
+local ShowUpdataInfo = true -- 可自行决定是否开启控制台输出显示,默认开启显示
+local CheckSteamID = true -- 可自行决定是否开启SteamID黑名单检测,默认开启检测
+local CheckRockstarID = true -- 可自行决定是否开启RockstarID黑名单检测,默认开启检测
 
 AddEventHandler("playerConnecting", function(name, reject, def)
 	local source = source
@@ -13,11 +18,23 @@ AddEventHandler("playerConnecting", function(name, reject, def)
 		return
 	end
 	
-	for k,v in ipairs(GetPlayerIdentifiers(source))do
-		if isBlacklisted(GetPlayerIdentifiers(source)[1]) then
-			reject("国服联合封禁：此Steam ID因违反规定已被本服务器或其他服务器永久封禁，禁止加入本服务器.(ID:" .. GetPlayerIdentifiers(source)[1] .. ")")
-			CancelEvent()
-			break
+	if CheckSteamID then
+		for k, v in ipairs(GetPlayerIdentifiers(source)) do
+			if isBlacklisted(GetPlayerIdentifiers(source)[1]) then
+				reject("国服联合封禁：此账户因违反规定已被本服务器或其他服务器联合永久封禁，故禁止加入本服务器. (Ban Type:0)") -- 如需修改请勿删除Ban Type提示,用于解封时区分类别
+				CancelEvent()
+				break
+			end
+		end
+	end
+	
+	if CheckRockstarID then
+		for k, v in ipairs(GetPlayerIdentifiers(source)) do
+			if isRockstarBlacklisted(GetPlayerIdentifiers(source)[2]) then
+				reject("国服联合封禁：此账户因违反规定已被本服务器或其他服务器联合永久封禁，故禁止加入本服务器. (Ban Type:1)") -- 如需修改请勿删除Ban Type提示,用于解封时区分类别
+				CancelEvent()
+				break
+			end
 		end
 	end
 end)
@@ -42,6 +59,16 @@ function isBlacklisted(id)
 	end
 end
 
+function isRockstarBlacklisted(id)
+	rblacklist = LoadResourceFile(GetCurrentResourceName(), "data/rockstarblacklist.txt") or ""
+	i,j = string.find(rblacklist, id)
+	if i ~= nil and j ~= nil then
+	    return true
+	else 
+	    return false
+	end
+end
+
 function CheckVersion()
 	if ShowUpdataInfo then
 		print("Checking version and blacklist...\n")
@@ -52,14 +79,26 @@ function CheckVersion()
 			if rText ~= localversion then
 				PerformHttpRequest(APIRequest .. "/data", function(err2, rText2, headers2)
 					if err2 == 200 then
-						SaveResourceFile(GetCurrentResourceName(), "data/version.txt", rText, -1)
 						SaveResourceFile(GetCurrentResourceName(), "data/blacklist.txt", rText2, -1)
 						APTRetryTimes = 0
-						if ShowUpdataInfo then
-							print("Successfully updated blacklist")
-							print("New Database Version:" .. tostring(rText))
-							print("Plugin will check it again after 120s")
-						end
+						PerformHttpRequest(APIRequest .. "/data2", function(err3, rText3, headers3)
+							if err3 == 200 then
+								SaveResourceFile(GetCurrentResourceName(), "data/version.txt", rText, -1)
+								SaveResourceFile(GetCurrentResourceName(), "data/rockstarblacklist.txt", rText3, -1)
+								APTRetryTimes = 0
+								if ShowUpdataInfo then
+									print("Successfully updated all blacklist")
+									print("New Database Version:" .. tostring(rText))
+									print("Plugin will check it again after 120s")
+								end
+							else
+								APTRetryTimes = APTRetryTimes + 1
+								if ShowUpdataInfo then
+									print("Failed to get rockstarblacklist")
+									print("Plugin will retry after 120s")
+								end
+							end
+						end)
 					else
 						APTRetryTimes = APTRetryTimes + 1
 						if ShowUpdataInfo then
